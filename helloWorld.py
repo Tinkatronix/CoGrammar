f676f2ea1b7e0ea16aa23f83ba60ec21ea22abb8
Tinkatronix <164647740+Tinkatronix@users.noreply.github.com>
2024-03-23 21:23:39 +0000

    Update helloWorld.py with comments
    Solve an issue with added comments for better readability.

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,3 @@
-inputString = str(input("Input some text to print. "))
-print(inputString) 
+# Code to practice with Git and GitHub
+inputString = str(input("Input some text to print. ")) # variable to take user input
+print(inputString) # print the user input
